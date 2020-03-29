bfe4234cbdc4d47a32281a1584d97987d9eabc7e
Gustavo Fernandes <gustavo@g13.com.br>
2020-03-29 17:45:47 -0300

    First Model-Controller-View - Department

--- WebSalesMVC/Controllers/HomeController.cs
+++ WebSalesMVC/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WebSalesMVC.Models;
+using WebSalesMVC.Models.ViewModels;
 
 namespace WebSalesMVC.Controllers {
     public class HomeController : Controller {
--- WebSalesMVC/Models/ViewModels/ErrorViewModel.cs
+++ WebSalesMVC/Models/ViewModels/ErrorViewModel.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace WebSalesMVC.Models {
+namespace WebSalesMVC.Models.ViewModels {
     public class ErrorViewModel {
         public string RequestId { get; set; }
 
